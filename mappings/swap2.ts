1def303780285a5ac526a8d992714a9e09d5eb71
JD <jackdoy99@icloud.com>
2023-04-04 21:04:02 +0100

    Updated swap2

--- mappings/swap2.ts
+++ mappings/swap2.ts
@@ -1,4 +1,4 @@
-import { Swap } from "../generated/Pair/PancakePair"
+import { Swap } from "../generated/PairV3/PancakePairV3"
 import { Swap as SwapSchema } from "../generated/schema"
 import { getUsdPricePerToken } from "./prices/index"
 import { Address } from "@graphprotocol/graph-ts"
@@ -9,17 +9,17 @@ import { RDNTToken } from "./constants"
 const BNBAddress = Address.fromHexString("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c")
 const RDNTAddress = Address.fromHexString("0xf7DE7E8A6bd59ED41a4b5fe50278b3B7f31384dF")
 
-export function handleSwap(event: Swap): void {
+export function handleSwapV3(event: Swap): void {
   let swap = new SwapSchema(event.transaction.hash.concatI32(event.logIndex.toI32()).toHexString())
 
   let token0 = BNBAddress
   let token1 = RDNTAddress
-  let amountBought = event.params.amount1Out
+  let amountBought = event.params.amount1
 
   if (BNBAddress.toHexString() > RDNTAddress.toHexString()) {
     token1 = BNBAddress
     token0 = RDNTAddress
-    amountBought = event.params.amount0Out
+    amountBought = event.params.amount0
   }
 
   if (amountBought <= BigInt.fromI32(0)) {
@@ -27,11 +27,11 @@ export function handleSwap(event: Swap): void {
   }
   swap.pair = event.address.toHexString()
   swap.timestamp = event.block.timestamp
-  swap.account = event.params.to.toHexString()
-  swap.amount0In = event.params.amount0In
-  swap.amount0Out = event.params.amount0Out
-  swap.amount1In = event.params.amount1In
-  swap.amount1Out = event.params.amount1Out
+  swap.account = event.params.sender.toHexString()
+  swap.amount0In = event.params.amount0
+  swap.amount0Out = event.params.amount0
+  swap.amount1In = event.params.amount1
+  swap.amount1Out = event.params.amount1
   let tokenPrice: BigDecimal
   let fetchPrice = getUsdPricePerToken(RDNTToken)
 
